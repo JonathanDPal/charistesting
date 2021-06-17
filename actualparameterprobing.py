0e1364240b8bd62154384d52ea7ba70a8df75485
JonathanDPal <jpal@nd.edu>
2021-06-17 19:12:24 -0400

    switched over to new object

--- actualparameterprobing.py
+++ actualparameterprobing.py
@@ -11,24 +11,24 @@ fake_fluxes = [5e-3, 1e-3, 5e-4, 1e-4, 5e-5]
 fake_seps = [20, 30, 40, 50, 60]
 
 # KLIP Parameters To Be Sampled
-annuli = [7,8,9]
-subsections = [3,4,5]
-movement = [1,2]
-numbasis = [10,20,30]
+annuli = 7
+subsections = 4
+movement = 1
+numbasis = 20
 
 # Filelist(s) & Associated Mask(s)
-hd1160_fileset = 'HD1160_cubes/*.fits'
-hd1160_mask = [144, 80]
+hr8799_fileset = 'HR8799_cubes/*.fits'
+hr8799_mask = None
 
 # Create TestDataset For Each Set of Observations, e.g. hd = TestDataset('hd1160/*.fits', ...)
-hd1160 = TestDataset(fileset=hd1160_fileset, object_name='HD1160', mask_xy=hd1160_mask,
+hr8799 = TestDataset(fileset=hr8799_fileset, object_name='HD1160', mask_xy=hr8799_mask,
                  fake_fluxes=fake_fluxes,fake_seps=fake_seps, annuli=annuli,
                  subsections=subsections, movement=movement, numbasis=numbasis)
 
 # Have TestDataset Go To Town
-hd1160.inject_fakes()
-hd1160.run_KLIP()
-hd1160.contrast_and_detection()
+hr8799.inject_fakes()
+hr8799.run_KLIP()
+hr8799.contrast_and_detection()
 
 # Print Out Time Taken
 end = time()
