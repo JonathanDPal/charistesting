54fb8785570398b893b0ff9dd90848ee963e1414
JonathanDPal <jpal@nd.edu>
2021-06-22 16:05:09 -0400

    trying to debug last little bit

--- actualparameterprobing.py
+++ actualparameterprobing.py
@@ -7,32 +7,40 @@ start = time()
 warnings.simplefilter('ignore', category=RuntimeWarning)
 
 # Describe Fake Planets To Be Injected
-fake_fluxes = [5e-3, 1e-3, 5e-4, 1e-4, 5e-5]
-fake_seps = [20, 30, 40, 50, 60]
+fake_fluxes = [5e-3, 5e-4, 5e-5]
+fake_seps = [20, 40, 60]
 
 # KLIP Parameters To Be Sampled
-annuli = [4, 6, 8, 10, 12]
-subsections = [2, 4, 6]
-movement = [0, 1, 2]
-numbasis = [10, 20, 30, 40, 50, 60]
-corr_smooth = [0, 1, 2]
+# annuli = [4, 6, 8, 10, 12]
+annuli = [6]
+# subsections = [2, 4, 6]
+subsections = [2]
+# movement = [0, 1, 2]
+movement = [0]
+# numbasis = [10, 20, 30, 40, 50, 60]
+numbasis = [20]
+# corr_smooth = [0, 1, 2]
+corr_smooth = [1]
 highpass = [False]
 spectrum = [None]
+mode = 'ADI+SDI'
 
 # Filelist(s) & Associated Mask(s)
-hr8799_fileset = 'HR8799_cubes/*.fits'
-hr8799_mask = None
+fileset = 'HR8799_cubes/*.fits'
+mask = None
+object_name = 'HR8799_mod2'
 
 # Create TestDataset For Each Set of Observations, e.g. hd = TestDataset('hd1160/*.fits', ...)
-hr8799 = TestDataset(fileset=hr8799_fileset, object_name='HR8799', mask_xy=hr8799_mask,
+td1 = TestDataset(fileset=fileset, object_name=object_name, mask_xy=mask,
                  fake_fluxes=fake_fluxes,fake_seps=fake_seps, annuli=annuli,
                  subsections=subsections, movement=movement, numbasis=numbasis,
-                     corr_smooth=corr_smooth, highpass=highpass, spectrum=spectrum)
+                     corr_smooth=corr_smooth, highpass=highpass, spectrum=spectrum, mode=mode,
+                  fake_PAs=[20,110,200,290])
 
 # Have TestDataset Go To Town
-# hr8799.inject_fakes()
-# hr8799.run_KLIP(run_on_fakes=False)
-hr8799.contrast_and_detection(detect_planets=False, calibrate=[False])
+td1.inject_fakes()
+td1.run_KLIP()
+td1.contrast_and_detection()
 
 # Print Out Time Taken
 end = time()
