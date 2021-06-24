04901e6fff20e891f34e023cf735aec816493afa
JonathanDPal <jpal@nd.edu>
2021-06-24 11:50:15 -0400

    automated fwhm calculation to inject planets with

--- actualparameterprobing.py
+++ actualparameterprobing.py
@@ -2,6 +2,8 @@ from parameter_test_infrastructure import *
 import warnings
 from time import time
 from numpy import floor
+from astropy.io import fits
+from glob import glob
 
 # Keeping Track of Time Taken
 start = time()
@@ -9,11 +11,18 @@ start = time()
 # KLIP often gets a bunch of RuntimeWarnings that we don't need to worry about
 warnings.simplefilter('ignore', category=RuntimeWarning)
 
+# Filelist(s) & Associated Mask(s) & Associated Name(s)
+fileset0 = 'HR8799_cubes/*.fits'
+mask0 = None
+object_name0 = 'HR8799_swappedWCS'
+with open(glob(fileset0)[0]) as hdulist:
+    filtname = hdulist[0].header['FILTNAME']
+
 # Describe Fake Planets To Be Injected
-fake_fluxes = [1e-4]
-fake_seps = [40]
-fake_fwhm = 6.01924560185 # broadband
-fake_PAs=[0]
+fake_fluxes = [1e-4, 1e-5, 1e-6]
+fake_seps = [20, 40, 60]
+fake_fwhm = FWHMIOWA_calculator(speccubefile=None, filtname=filtname)[0]
+fake_PAs=[0, 90, 180, 270]
 
 # KLIP Parameters To Be Sampled
 # annuli = [4, 6, 8, 10, 12]
@@ -27,14 +36,9 @@ numbasis = [20]
 # corr_smooth = [0, 1, 2]
 corr_smooth = [1]
 highpass = [True]
-# highpass = [False, 5.0, True, 15.0]
+# highpass = [False, 5.0, True, 15.0] # True yields default (10.0)
 spectrum = [None]
 mode = 'ADI+SDI'
-
-# Filelist(s) & Associated Mask(s) & Associated Name(s)
-fileset0 = 'HR8799_cubes/*.fits'
-mask0 = None
-object_name0 = 'HR8799_1pt'
 
 # Create TestDataset For Each Set of Observations
 td0 = TestDataset(fileset=fileset0, object_name=object_name0, mask_xy=mask0, fake_fluxes=fake_fluxes,
@@ -44,8 +48,8 @@ td0 = TestDataset(fileset=fileset0, object_name=object_name0, mask_xy=mask0, fak
 
 # Have TestDataset Run Each Part
 td0.inject_fakes()
-td0.run_KLIP(run_on_nofakes=False)
-td0.contrast_and_detection(calibrate=[True])
+td0.run_KLIP()
+td0.contrast_and_detection()
 
 # Print Out Time Taken
 end = time()
@@ -55,4 +59,6 @@ remaining_time = time_elapsed - (hours * 3600)
 minutes = int(floor(remaining_time / 60))
 seconds = round(remaining_time - minutes * 60)
 
-print(f"##################### TIME ELAPSED: {hours} Hours, {minutes} Minutes, {seconds} Seconds #####################")
+print("##################### TIME ELAPSED: {0} Hours, {1} Minutes, {2} Seconds #####################".format(hours,
+                                                                                                             minutes,
+                                                                                                             seconds))
