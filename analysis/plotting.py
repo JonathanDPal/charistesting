990f5cecad9805779accb37ac062604c554a62fc
JonathanDPal <jpal@nd.edu>
2021-07-06 13:57:16 -0400

    making more generalizable

--- analysis/plotting.py
+++ analysis/plotting.py
@@ -5,7 +5,6 @@ import numpy as np
 from glob import glob
 import os
 
-snr_values = np.arange(start=3, stop=10, step=0.5)
 
 def roc_generator(snr_values, vals_and_finders, filepath_to_save):
     """
@@ -13,10 +12,11 @@ def roc_generator(snr_values, vals_and_finders, filepath_to_save):
     ---
     Args:
         snr_values (list): SNR Values to plot
-        vals_and_finders (dict): Values to check outare keys and file finders are the values.
+        vals_and_finders (dict): Values to check out are keys and file finders are the values.
         filepath_to_save (str): Where to save graph.
     """
     collection = dict()
+
     for val in vals_and_finders.keys():
         collection[val] = dict()
         for snr_value in snr_values:
@@ -25,10 +25,11 @@ def roc_generator(snr_values, vals_and_finders, filepath_to_save):
         for file in val_fileset:
             detections = pd.read_csv(file)
             for snr_value in snr_values:
-                detections_subset = detections[detections['SNR Value'] >= snr_value]
-                relevant_column = detections_subset['Injected']
-                collection[val][snr_value].append([np.sum(relevant_column), len(relevant_column) -
-                                               np.sum(relevant_column)])
+                detections_subset = detections[detections['SNR Value'] >= snr_value and detections['Injected'] !=
+                                               "Science Target"]
+                detections['Injected'] = [True if x == "True" else False for x in detections['Injected']]
+                inj = detections_subset['Injected']
+                collection[val][snr_value].append([np.sum(inj), len(inj) - np.sum(inj)])
 
     for val in vals_and_finders.keys():
         k = collection[val]
@@ -59,18 +60,43 @@ def contrast_curve(vals_and_files, filepath_to_save):
     plt.savefig(filepath_to_save)
 
 
-def heatmap(annuli, movement, directory_switcher, filepath_to_save, file_finder='*.csv'):
+def max_value_heatmap(param1, param2, directory_switcher, filepath_to_save, file_finder='*.csv'):
     """
-    For now, just shows the maximum SNR value found
+    For now, just shows the maximum SNR value found (i.e. SNR of HD1160)
     """
     os.chdir(directory_switcher)
     fileset = glob(file_finder)
-    full_data = {a: {m:[] for m in movement} for a in annuli}
+    full_data = {str(a): {str(m):[] for m in param2} for a in param1}
     for file in fileset:
         df = pd.read_csv(file)
         snr = df['SNR Value'].max()
-        ani = int(file[0])
-        mov = int(file[21])
+        ani = file[0]
+        mov = file[21]
+        full_data[ani][mov].append(snr)
+    for ani in param1:
+        for mov in param2:
+            full_data[ani][mov] = np.mean(full_data[ani][mov])
+    plot_snr = []
+    for mov in param2:
+        plot_snr.append([full_data[ani][mov] for ani in param1])
+    data_to_plot = pd.DataFrame(plot_snr, index=param2, columns=param1)
+    sns.heatmap(data_to_plot, annot=True, linewidths=0.2, fmt='d')
+    plt.savefig(filepath_to_save)
+
+
+def max_value_heatmap(annuli, movement, directory_switcher, filepath_to_save, file_finder='*.csv'):
+    """
+    For now, just shows the maximum SNR value found (i.e. SNR of HD1160)
+    """
+    os.chdir(directory_switcher)
+    fileset = glob(file_finder)
+    full_data = {str(a): {str(m):[] for m in movement} for a in annuli}
+    for file in fileset:
+        df = pd.read_csv(file)
+        science_target = df[df["Injected"] == "Science Target"]
+        snr = science_target['SNR Value'].max()
+        ani = file[0]
+        mov = file[21]
         full_data[ani][mov].append(snr)
     for ani in annuli:
         for mov in movement:
@@ -81,3 +107,49 @@ def heatmap(annuli, movement, directory_switcher, filepath_to_save, file_finder=
     data_to_plot = pd.DataFrame(plot_snr, index=movement, columns=annuli)
     sns.heatmap(data_to_plot, annot=True, linewidths=0.2, fmt='d')
     plt.savefig(filepath_to_save)
+
+
+def mean_injection_value_heatmap(annuli, movement, directory_switcher, filepath_to_save, file_finder='*.csv'):
+    """
+    For now, just shows the maximum SNR value found (i.e. SNR of HD1160)
+    """
+    os.chdir(directory_switcher)
+    fileset = glob(file_finder)
+    full_data = {str(a): {str(m):[] for m in movement} for a in annuli}
+    for file in fileset:
+        df = pd.read_csv(file)
+        injected = df[df["Injected"] == "True"]
+        snr = injected['SNR Value'].mean()
+        ani = file[0]
+        mov = file[21]
+        full_data[ani][mov].append(snr)
+    for ani in annuli:
+        for mov in movement:
+            full_data[ani][mov] = np.mean(full_data[ani][mov])
+    plot_snr = []
+    for mov in movement:
+        plot_snr.append([full_data[ani][mov] for ani in annuli])
+    data_to_plot = pd.DataFrame(plot_snr, index=movement, columns=annuli)
+    sns.heatmap(data_to_plot, annot=True, linewidths=0.2, fmt='d')
+    plt.savefig(filepath_to_save)
+
+
+def valuefinder(filename, param):
+    param = str.lower(param)
+    paramlengths = {'annuli': 6, 'subsections': (11, int), 'movement': (8, int), 'spectrum': (8, NoneType),
+                    'smooth': 6, 'highpass': 8, 'KL': 4}
+    paramlength = paramlengths[param][0]
+    startingindex = None # will be defined soon
+    for i in range(len(filename)):
+        if str.lower(filename[i:i+paramlength]) == param:
+            startingindex = i - 1
+
+    valuelength = 0
+    while startingindex >= 0 and filename[startingindex] != '_':
+        startingindex -= 1
+        valuelength += 1
+    value = filename[startingindex:startingindex+valuelength]
+
+    return value
+
+
