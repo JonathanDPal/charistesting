ea95cd48001e2a9bfb41f887b91eb24240bdfc01
JonathanDPal <jpal@nd.edu>
2021-07-13 01:32:49 -0400

    no longer need to specify object_name -- this will get changed tomorrow to be more generalized

--- analysis/analysis.sh
+++ analysis/analysis.sh
@@ -1,5 +1,5 @@
-object_name=$1
-num_injections=$2
+object_name=$(cut -d / -f 7 <<< "$PWD")
+num_injections=$1
 
 python roc.py "$object_name" highpass "$num_injections"
 python roc.py "$object_name" smooth "$num_injections"
