abd35752a7d3d4a242a9fc939e064da4ec2fdc0f
JonathanDPal <jpal@nd.edu>
2021-07-13 10:10:03 -0400

    generalized object_name!!!

--- analysis/analysis.sh
+++ analysis/analysis.sh
@@ -1,12 +1,11 @@
-object_name=$(cut -d / -f 7 <<< "$PWD")
-num_injections=$1
+object_name=$( (rev | cut -d / -f 2 | rev) <<< "$PWD")
 
-python roc.py "$object_name" highpass "$num_injections"
-python roc.py "$object_name" smooth "$num_injections"
-python roc.py "$object_name" numbasis "$num_injections"
+python roc.py "$object_name" highpass
+python roc.py "$object_name" smooth
+python roc.py "$object_name" numbasis
 python maxheatmap.py "$object_name" annuli subsections
 python maxheatmap.py "$object_name" annuli movement
 python maxheatmap.py "$object_name" subsections movement
-python meanheatmap.py "$object_name" annuli subsections "$num_injections"
-python meanheatmap.py "$object_name" annuli movement "$num_injections"
-python meanheatmap.py "$object_name" subsections movement "$num_injections"
+python meanheatmap.py "$object_name" annuli subsections
+python meanheatmap.py "$object_name" annuli movement
+python meanheatmap.py "$object_name" subsections movement
