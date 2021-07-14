057103fddfcd5279c94df306c1ba952ff8fabfe5
JonathanDPal <jpal@nd.edu>
2021-07-13 23:53:17 -0400

    inactive changes only -- just fixing up formatting to match PEP 8

--- HDparameterprobing.py
+++ HDparameterprobing.py
@@ -8,9 +8,9 @@ import os
 from astropy.wcs.wcs import FITSFixedWarning
 import sys
 
-#####################################################
-#################### USER INPUTS ####################
-#####################################################
+###############
+# USER INPUTS #
+###############
 
 # see https://docs.google.com/document/d/1yX0l96IZs1IxxKCRmriVSAQM3KFGF9U1-FnpJXhcLXo/edit?usp=sharing for help
 
@@ -20,41 +20,42 @@ mask0 = [144, 80]
 object_name0 = 'HD1160'
 
 # Setting Up Lists/Tuples For KLIP
-annuli = [4, 6, 8, 10, 12] # List of Integer(s)
-subsections = [2, 4, 6] # List of Integer(s)
-movement = [0, 1, 2] # List of Float(s)
-spectrum = [None] # List of None and/or 'methane'
-numbasis = [10, 20, 30, 40, 50, 60] # List of Integer(s)
-corr_smooth = [0, 1, 2] # List of Float(s) and/or Integer(s)
-highpass = [False, 5.0, True, 15.0] # List of Float(s), Integer(s), and/or Bool(s)
+annuli = [4, 6, 8, 10, 12]  # List of Integer(s)
+subsections = [2, 4, 6]  # List of Integer(s)
+movement = [0, 1, 2]  # List of Float(s)
+spectrum = [None]  # List of None and/or 'methane'
+numbasis = [10, 20, 30, 40, 50, 60]  # List of Integer(s)
+corr_smooth = [0, 1, 2]  # List of Float(s) and/or Integer(s)
+highpass = [False, 5.0, True, 15.0]  # List of Float(s), Integer(s), and/or Bool(s)
 
 # Setting Mode For KLIP
-mode = 'ADI+SDI' # Exactly ONE (not a list or tuple) or the following: 'ADI', 'SDI', 'ADI+SDI'
+mode = 'ADI+SDI'  # Exactly ONE (not a list or tuple) or the following: 'ADI', 'SDI', 'ADI+SDI'
 
 # Maximum Number of Threads to Use (set to None to use maximum computer capacity)
 max_numthreads = None
 
 # Setting Up For Fake Planets
-fake_fluxes = [5e-4, 5e-5, 5e-6, 1e-4, 1e-5, 1e-6] # List of Float(s)
-fake_seps = [20, 40, 60] # List of Integer(s) and/or Float(s)
-fake_PAs=[19, 79, 139, 199, 259, 319] # List of Integer(s) and/or Float(s)
+fake_fluxes = [5e-4, 5e-5, 5e-6, 1e-4, 1e-5, 1e-6]  # List of Float(s)
+fake_seps = [20, 40, 60]  # List of Integer(s) and/or Float(s)
+fake_PAs = [19, 79, 139, 199, 259, 319]  # List of Integer(s) and/or Float(s)
 
-##### Specifying Which Things to Do/Not Do #####
+# Specifying Which Things to Do/Not Do #
 # Most of the time, the four values below should be set to True
 put_in_fakes = True
-run_KLIP_on_dataset_with_fakes = False # if no fakes are injected, this will just be a dataset without fakes
-get_contrast = True # won't be calibrated if no fake planets are injected
+run_KLIP_on_dataset_with_fakes = False  # if no fakes are injected, this will just be a dataset without fakes
+get_contrast = True  # won't be calibrated if no fake planets are injected
 get_planet_detections_from_dataset_with_fakes = False
 # Most of the time, these two values below should be set to False
 run_KLIP_on_dataset_without_fakes = False
 get_planet_detections_from_dataset_without_fakes = False
 
-############################################################
-#################### END OF USER INPUTS ####################
-############################################################
+######################
+# END OF USER INPUTS #
+######################
 
-
-################## CHECKING USER INPUTS #################
+########################
+# CHECKING USER INPUTS #
+########################
 
 # Warning User if the Directory Where Stuff Will Be Outputted to Already Exists
 if os.path.exists(object_name0):
@@ -71,14 +72,9 @@ for param in [[annuli, 'annuli'], [subsections, 'subsections'], [movement, 'move
 
 # Checking Mode -- Common Mistake is Inputting Mode as a List/Tuple Like Other Params
 if not isinstance(mode, str):
-    warnings.warn("WARNING: Inputted mode is not a string. If inputted mode is a list or tuple, then script"
-                  "will proceed using index 0 value as the mode. Any other values will not be used.")
-    try:
-        mode = mode[0]
-    except:
-        raise TypeError("Mode needs to be a string. Check input. See "
-                        "https://docs.google.com/document/d/1yX0l96IZs1IxxKCRmriVSAQM3KFGF9U1-FnpJXhcLXo/edit?usp"
-                        "=sharing for help")
+    raise TypeError("Mode needs to be a string. Check input. See "
+                    "https://docs.google.com/document/d/1yX0l96IZs1IxxKCRmriVSAQM3KFGF9U1-FnpJXhcLXo/edit?usp"
+                    "=sharing for help")
 
 # Checking Fake Planet Stuff
 for param in [[fake_fluxes, 'fake_fluxes'], [fake_seps, 'fake_seps'], [fake_PAs, 'fake_PAs']]:
@@ -89,7 +85,7 @@ for param in [[fake_fluxes, 'fake_fluxes'], [fake_seps, 'fake_seps'], [fake_PAs,
         if put_in_fakes:
             raise ValueError("put_in_fakes is set to true, but {0} is not a list or tuple.".format(param[1]))
 
-##### SYNTHESIZING USER INPUTS INTO A COUPLE ADDITIONAL BOOLEANS ######
+# SYNTHESIZING USER INPUTS INTO A COUPLE ADDITIONAL BOOLEANS #
 detect_planets = get_planet_detections_from_dataset_with_fakes or get_planet_detections_from_dataset_without_fakes
 if put_in_fakes:
     datasetwithfakes = True
@@ -114,9 +110,9 @@ else:
     batched = False
     paramset = None
 
-############################################################
-################ ACTUALLY STARTING TESTING #################
-############################################################
+#############################
+# ACTUALLY STARTING TESTING #
+#############################
 start0 = time()
 
 # KLIP creates a bunch of RuntimeWarnings that we don't want to spam our log file
@@ -125,7 +121,7 @@ warnings.simplefilter('ignore', category=FITSFixedWarning)
 
 # FWHM Is Based on Central Wavelength of Filter Used During Obsveration.
 with fits.open(glob(fileset0)[0]) as hdulist:
-   fake_fwhm0 = FWHMIOWA_calculator(hdulist)[0]
+    fake_fwhm0 = FWHMIOWA_calculator(hdulist)[0]
 
 # Create TestDataset For Target 0
 td0 = TestDataset(fileset=fileset0, object_name=object_name0, mask_xy=mask0, fake_fluxes=fake_fluxes,
@@ -154,4 +150,4 @@ minutes = int(floor(remaining_time / 60))
 seconds = round(remaining_time - minutes * 60)
 
 td0.write_to_log_and_print("##################### TIME ELAPSED: {0} Hours, {1} Minutes, {2} Seconds "
-                            "#####################".format(hours, minutes, seconds))
+                           "#####################".format(hours, minutes, seconds))
