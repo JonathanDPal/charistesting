d03f86481dc2b88dd4e63b88d582c524aa4115a9
JonathanDPal <jpal@nd.edu>
2021-07-27 14:09:43 -0400

    little script that tests to see how many values are greater than or less than zero. previously was just doing this over and over in ipython so wrote a script for it

---
+++ effec.py
@@ -0,0 +1,25 @@
+import sys
+import pandas as pd
+from glob import glob
+
+direc = sys.argv[1]
+files = glob(f'{direc}/*.xlsx')
+
+good = 0
+bad = 0
+
+for file in files:
+	badfile = False
+	df = pd.read_excel(file)
+	for col in df.columns:
+		if 'Retrieved Flux' in col:
+			for elm in df[col]:
+				if elm > 0:
+					good += 1
+				else:
+					bad += 1
+					badfile = True
+	if not badfile:
+		print(file)
+
+print(f'Effectiveness: {round(float(good) / (float(good) + float(bad)) * 100, 2)}')
