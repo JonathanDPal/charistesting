32aab1e78d093a84dbe28fa5425474564b93397d
JonathanDPal <jpal@nd.edu>
2021-11-02 19:31:14 -0400

    using medians by default for weighting

--- analysis/overall_numericalscoring.py
+++ analysis/overall_numericalscoring.py
@@ -2,12 +2,16 @@ import pandas as pd
 import numpy as np
 import sys
 
-snrweight, contrastweight = sys.argv[1], sys.argv[2]
-
 snr = pd.read_csv('numericalscoring/snr_scores.csv')
 contrast = pd.read_csv('numericalscoring/contrast_scores.csv')
 del snr['Unnamed: 0']
 del contrast['Unnamed: 0']
+
+if len(sys.argv) > 1:
+    snrweight, contrastweight = sys.argv[1], sys.argv[2]
+else:
+    # this should essentially allow them to be weighted equally
+    snrweight, contrastweight = contrast['Score'].median(), snr['Score'].median()
 
 overall = snr.merge(contrast, how='outer', on=['Annuli', 'Subsections', 'Movement', 'Spectrum', 'Numbasis',
                                                'Corr_Smooth', 'Highpass'], suffixes=['_snr', '_contrast'])
