fe8c5a07a2ef208f2835b7a342f1fe390c40920a
JonathanDPal <jpal@nd.edu>
2022-05-30 16:53:15 -0700

    splitting between data1 and data0

--- organizational_scripts/move_and_delete.py
+++ organizational_scripts/move_and_delete.py
@@ -4,11 +4,25 @@ import os
 
 direc = sys.argv[1]
 klipfiles = glob(f'{direc}/klipped_cubes_W_fakes/*.fits')
+klipnofakesfiles = glob(f'{direc}/klipped_cubes_N_fakes/*.fits')
 contrastfiles = glob(f'{direc}/calibrated_contrast/*.csv')
+uncalcontrastfiles = glob(f'{direc}/uncalibrated_contrast/*.csv')
 detectionsfiles = glob(f'{direc}/detections/*.csv')
 
-files = klipfiles + contrastfiles + detectionsfiles
+files = klipfiles + klipnofakesfiles + contrastfiles + uncalcontrastfiles + detectionsfiles
 for file in files:
-    os.system(f'rsync -caqz {file} jpal@planetfinder.esc.nd.edu:/home/jpal/data0/jpal/parameter_sampling/'
-              f'{direc}/from_crc')
-    os.remove(file)
+    subdirec = file.split('/')[-2]
+    if subdirec in ['klipped_cubes_W_fakes', 'klipped_cubes_N_fakes']:
+        try:
+            assert os.path.exists(file)
+            os.system(f'rsync -caqz {file} jpal@planetfinder.esc.nd.edu:/home/jpal/data1/jpal/{direc}/{subdirec}')
+            os.remove(file)
+        except (AssertionError, FileNotFoundError) as e:
+            pass
+    else:
+        try:
+            assert os.path.exists(file)
+            os.system(f'rsync -caqz {file} jpal@planetfinder.esc.nd.edu:/home/jpal/data0/jpal/{direc}/{subdirec}')
+            os.remove(file)
+        except (AssertionError, FileNotFoundError) as e:
+            pass
