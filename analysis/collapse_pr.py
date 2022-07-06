0c734f1acb4f984e19beeb39bfd0b1314817bcf9
JonathanDPal <jpal@nd.edu>
2022-07-06 12:55:47 -0400

    customizable num to collapse to

--- analysis/collapse_pr.py
+++ analysis/collapse_pr.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import itertools
+import sys
 
 df = pd.read_csv('paramrankings_indsep.csv')
 annuli = np.arange(12) + 1
@@ -9,6 +10,7 @@ movement = [float(x) for x in np.arange(6)]
 corr_smooth = [float(x) for x in np.arange(4)]
 highpass = ['False', '15.0', '30.0']
 newdf = pd.DataFrame(columns=[col for col in df.columns if col != 'Numbasis'])
+numnb_touse = int(sys.argv[1])
 
 stars, nums = ['HD1160', 'HR8799', 'KB', 'Kappa'], [13, 20, 30, 40, 50, 60, 'SciSNR']
 columnnames = [f'{star}_{num}' for star in stars for num in nums]
@@ -25,10 +27,10 @@ for ann in annuli:
                     subdf = subdf[subdf['Corr_Smooth'] == cs]
                     subdf = subdf[subdf['Highpass'] == hp]
                     assert len(subdf) == 6
-                    cbs = [cbo for cbo in itertools.combinations(np.arange(6), 4)]
+                    cbs = [cbo for cbo in itertools.combinations(np.arange(6), numnb_touse)]
                     tholds = [np.min([subdf.iloc[list(cbo), :][col].max() for col in columnnames]) for cbo in cbs]
                     ssdf = subdf.iloc[list(cbs[np.argmax(tholds)]), :]
-                    assert len(ssdf) == 4
+                    assert len(ssdf) == numnb_touse
                     newrow = [ssdf[col][ssdf.index[0]] for col in pcols]
                     maxes = [ssdf[col].max() for col in columnnames]
                     newrow += maxes
@@ -36,4 +38,4 @@ for ann in annuli:
                     newdf.loc[len(newdf.index)] = newrow
 
 newdf = newdf.sort_values('Average', ascending=False)
-newdf.to_csv('paramrankings_indsep.csv', index=False)
+newdf.to_csv(f'paramrankings/paramrankings_indsep_nb{numnb_touse}.csv', index=False)
