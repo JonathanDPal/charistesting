f652838741cdddbc13745868cfa0587967e6c5fa
JonathanDPal <jpal@nd.edu>
2022-07-08 15:04:30 -0400

    for early on

--- analysis/collapse_batches.py
+++ analysis/collapse_batches.py
@@ -4,9 +4,11 @@ import sys
 import os
 
 batchfiles = glob(sys.argv[1])
-df = pd.read_csv(batchfiles[0])
-for bf in batchfiles[1:]:
-    df = pd.concat([df, pd.read_csv(bf)], ignore_index=True)
-max_thold = df['Threshold'].max()
-df = df[df['Threshold'] == max_thold]
-df.to_csv(sys.argv[2], index=False)
+if len(batchfiles) > 0:
+    df = pd.read_csv(batchfiles[0])
+    if len(batchfiles) > 1:
+        for bf in batchfiles[1:]:
+            df = pd.concat([df, pd.read_csv(bf)], ignore_index=True)
+        max_thold = df['Threshold'].max()
+        df = df[df['Threshold'] == max_thold]
+    df.to_csv(sys.argv[2], index=False)
