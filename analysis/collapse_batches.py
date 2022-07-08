a649a9cc478c473495e059b9a767df18819480e6
JonathanDPal <jpal@nd.edu>
2022-07-08 02:16:31 -0400

    delete extraneous

--- analysis/collapse_batches.py
+++ analysis/collapse_batches.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from glob import glob
 import sys
+import os
 
 batchfiles = glob(sys.argv[1])
 df = pd.read_csv(batchfiles[0])
-for bf in batchfiles:
+for bf in batchfiles[1:]:
     df = df.append(bf)
 max_thold = df['Threshold'].max()
 df = df[df['Threshold'] == max_thold]
 df.to_csv(sys.argv[2], index=False)
+for bf in batchfiles:
+    os.remove(bf)
