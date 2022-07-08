803555317e674dccc695ee99f738e22ef95de8b6
JonathanDPal <jpal@nd.edu>
2022-07-08 15:55:37 -0400

    added parallelization option

--- analysis/opis.py
+++ analysis/opis.py
@@ -3,17 +3,23 @@ import sys
 import pandas as pd
 import itertools
 from copy import copy
+from multiprocessing.pool import Pool
 
 N = int(sys.argv[1])  # how big of parameter sets we're checking
 csvfile = sys.argv[2]  # where the ranking information is coming from
 output_filename = sys.argv[3]
 num_to_skip = int(sys.argv[4])  # ones that have already been checked
 max_threshold = float(sys.argv[5]) if '.csv' not in sys.argv[5] else sys.argv[5]  # start at current known value
-if len(sys.argv) == 8:
+if len(sys.argv) >= 8:
     batched = True
     batchindex, batchsize = int(sys.argv[6]), int(sys.argv[7])
 else:
     batched = False
+if len(sys.argv) == 9:
+    parallelize = True
+    num_cores = int(sys.argv[8])
+else:
+    parallelize = False
 
 if isinstance(max_threshold, str):
     max_threshold = pd.read_csv(max_threshold)['Threshold'][0]
@@ -43,50 +49,107 @@ if not batched:  # don't want a bunch of print statements on the batched stuff
     print(f'{numindexcombos} combinations will be checked.')
 data_to_save = {'Params': list(), 'Threshold': list()}
 
-for idx, ic in enumerate(indexcombos):
-    ic = list(ic)
-    subdf = df.iloc[ic, :]
-    if subdf['max'].min() < max_threshold:
-        continue
-    colmaxes = pd.DataFrame({'max': [np.max(subdf[col]) for col in columnnames]}, index=columnnames).sort_values('max')
-    curr = np.max(ic) + 1
-    for k in np.arange(numrows - curr) + curr:
-        found = 0
-        if found < N - 1 and k in ic:
-            found += 1
+if not parallelize:
+    for ic in indexcombos:
+        ic = list(ic)
+        subdf = df.iloc[ic, :]
+        if subdf['max'].min() < max_threshold:
             continue
-        ssdf = df.iloc[k, :]
-        if ssdf['max'] < max_threshold:
-            continue
-        local_cm = copy(colmaxes)
-        threshold = local_cm['max'][0]
-        for col, row in local_cm.iterrows():
-            new_val = ssdf[col]
-            old_val = row['max']
-            if new_val < old_val:
-                if old_val < max_threshold:
-                    break
-            else:
-                if new_val < max_threshold:
-                    break
+        colmaxes = pd.DataFrame({'max': [np.max(subdf[col]) for col in columnnames]},
+                                index=columnnames).sort_values('max')
+        curr = np.max(ic) + 1
+        for k in np.arange(numrows - curr) + curr:
+            found = 0
+            if found < N - 1 and k in ic:
+                found += 1
+                continue
+            ssdf = df.iloc[k, :]
+            if ssdf['max'] < max_threshold:
+                continue
+            local_cm = copy(colmaxes)
+            threshold = local_cm['max'][0]
+            for col, row in local_cm.iterrows():
+                new_val = ssdf[col]
+                old_val = row['max']
+                if new_val < old_val:
+                    if old_val < max_threshold:
+                        break
                 else:
-                    row['max'] = new_val
-                    threshold = local_cm['max'].min()
-        if threshold > max_threshold:
-            ssubdf = copy(subdf)
-            ssubdf.loc[len(ssubdf.index)] = ssdf
-            ssubdf.index = np.arange(N)
-            paramgroups = [tuple(ssubdf.iloc[idx, :5]) for idx in range(N)]
-            data_to_save['Params'] = [str(paramgroups)]
-            data_to_save['Threshold'] = [threshold]
-            max_threshold = threshold
-        elif threshold == max_threshold:
-            ssubdf = copy(subdf)
-            ssubdf.loc[len(ssubdf.index)] = ssdf
-            ssubdf.index = np.arange(N)
-            paramgroups = [tuple(ssubdf.iloc[idx, :5]) for idx in range(N)]
-            data_to_save['Params'].append(str(paramgroups))
-            data_to_save['Threshold'].append(threshold)
+                    if new_val < max_threshold:
+                        break
+                    else:
+                        row['max'] = new_val
+                        threshold = local_cm['max'].min()
+            if threshold > max_threshold:
+                ssubdf = copy(subdf)
+                ssubdf.loc[len(ssubdf.index)] = ssdf
+                ssubdf.index = np.arange(N)
+                paramgroups = [tuple(ssubdf.iloc[idx, :5]) for idx in range(N)]
+                data_to_save['Params'] = [str(paramgroups)]
+                data_to_save['Threshold'] = [threshold]
+                max_threshold = threshold
+            elif threshold == max_threshold:
+                ssubdf = copy(subdf)
+                ssubdf.loc[len(ssubdf.index)] = ssdf
+                ssubdf.index = np.arange(N)
+                paramgroups = [tuple(ssubdf.iloc[idx, :5]) for idx in range(N)]
+                data_to_save['Params'].append(str(paramgroups))
+                data_to_save['Threshold'].append(threshold)
+else:
+    def combo_threshold(ic, m_threshold=max_threshold):
+        ic = list(ic)
+        subdf = df.iloc[ic, :]
+        if subdf['max'].min() < m_threshold:
+            return None, None
+        colmaxes = pd.DataFrame({'max': [np.max(subdf[col]) for col in columnnames]}, index=columnnames).sort_values(
+            'max')
+        curr = np.max(ic) + 1
+
+        prms, threshold = None, None
+        for k in np.arange(numrows - curr) + curr:
+            found = 0
+            if found < N - 1 and k in ic:
+                found += 1
+                continue
+            ssdf = df.iloc[k, :]
+            if ssdf['max'] < m_threshold:
+                continue
+            local_cm = copy(colmaxes)
+            threshold = local_cm['max'][0]
+            for col, row in local_cm.iterrows():
+                new_val = ssdf[col]
+                old_val = row['max']
+                if new_val < old_val:
+                    if old_val < m_threshold:
+                        break
+                else:
+                    if new_val < m_threshold:
+                        break
+                    else:
+                        row['max'] = new_val
+                        threshold = local_cm['max'].min()
+            if threshold > m_threshold:
+                ssubdf = copy(subdf)
+                ssubdf.loc[len(ssubdf.index)] = ssdf
+                ssubdf.index = np.arange(N)
+                paramgroups = [tuple(ssubdf.iloc[idx, :5]) for idx in range(N)]
+                prms = [str(paramgroups)]
+                m_threshold = threshold
+            elif threshold == m_threshold:
+                ssubdf = copy(subdf)
+                ssubdf.loc[len(ssubdf.index)] = ssdf
+                ssubdf.index = np.arange(N)
+                paramgroups = [tuple(ssubdf.iloc[idx, :5]) for idx in range(N)]
+                prms.append(str(paramgroups))
+        return prms, threshold
+
+    with Pool(num_cores) as p:
+        result = list(p.map(combo_threshold, indexcombos, int(numindexcombos / (num_cores * 2))))
+    r_params = [r[0] for r in result if r[0] is not None and r[1] is not None]
+    r_threshold = [r[1] for r in result if r[0] is not None and r[1] is not None]
+    top_indices = np.array(np.nonzero(r_threshold == np.max(r_threshold))).flatten()
+    data_to_save['Params'] = [r_params[idx] for idx in top_indices]
+    data_to_save['Threshold'] = [r_threshold[idx] for idx in top_indices]
 
 
 def break_up_params(pset):
